fa60d3423ad3fc76771e6c604f1196c4ee0a8294
PulseX-executor <nopphon.vij@gmail.com>
2024-08-28 15:50:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// Wait for the page to load
+window.addEventListener('load', function() {
+    // Hide the loading overlay
+    const loadingOverlay = document.getElementById('loading-overlay');
+    loadingOverlay.style.opacity = '0';
+    loadingOverlay.style.visibility = 'hidden';
+
+    // Show the main content
+    const mainContent = document.getElementById('main-content');
+    mainContent.style.opacity = '1';
+});
+
+function downloadLatestVersion() {
+    fetch('https://raw.githubusercontent.com/Kob123678/Kob123678.github.io/main/links/pulsex.txt')
+        .then(response => response.text())
+        .then(url => window.open(url.trim(), '_blank'));
+}
+
+function downloadOpenSource() {
+    fetch('https://raw.githubusercontent.com/Kob123678/Kob123678.github.io/main/links/source.txt')
+        .then(response => response.text())
+        .then(url => window.open(url.trim(), '_blank'));
+}
+
+function moreInformation() {
+    window.open('https://github.com/Kob123678/PulseX-v1', '_blank');
+}
+
+// Theme toggle functionality
+const themeToggleSwitch = document.getElementById('themeToggleSwitch');
+
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "light") {
+    document.documentElement.setAttribute("data-theme", "light");
+    themeToggleSwitch.checked = false;
+} else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    themeToggleSwitch.checked = true;
+}
+
+themeToggleSwitch.addEventListener('change', () => {
+    const theme = themeToggleSwitch.checked ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+});
